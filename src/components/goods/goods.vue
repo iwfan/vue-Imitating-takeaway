6e946bf929c964ffdeaefc6d033b6ec905262996
wangfan <i.wangfan@foxmail.com>
2018-05-28 23:18:04 +0800

    update

--- src/components/goods/goods.vue
+++ src/components/goods/goods.vue
@@ -1,14 +1,36 @@
 <template lang="pug">
-  div 商品页
+  .goods
+    .menu-wrapper
+    .foods-wrapper
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
+import axios from 'axios'
 export default Vue.extend({
-  name: 'goods'
+  name: 'goods',
+  props: {
+    seller: {
+      type: Object
+    }
+  },
+  data () {
+    return {
+      goods: []
+    }
+  },
+  created () {
+    axios('/api/goods').then(({data}) => {
+      this.goods = data.data
+    })
+  }
 })
 </script>
 
 <style lang="stylus" scoped>
+  .goods
+    display flex
+    position absolute
+    top 174px
 
 </style>
